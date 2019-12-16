e9a17df780d6b93443079530dab736d0e2a3bd17
Ivan Radonov <iradonov@techfak.uni-bielefeld.de>
2019-12-16 20:16:44 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='PHMM',
+      version='0.0.1',
+      description='Profile Hidden Markov Models',
+      author='Ivan Radonov',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
