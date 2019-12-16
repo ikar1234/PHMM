7800c78d5cffed80dac431a0d7751ef2cb276948
Ivan Radonov <iradonov@techfak.uni-bielefeld.de>
2019-12-16 18:44:29 +0100

    add init

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python test\\GenericTest.py
-python test\\MainTest.py
-python test\\RecordsTest.py
-python test\\ParseTest.py
+python test/GenericTest.py
+python test/MainTest.py
+python test/RecordsTest.py
+python test/ParseTest.py
